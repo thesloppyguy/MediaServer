43453b485813c46b5adc16bf5321a29eb302b94d
thesloppyguy <sahilmandal@outlook.com>
2023-06-04 02:40:16 +0530

    Navbar

---
+++ frontend/src/components/CardsDeal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardsDeal = () => {
+  return <div>CardsDeal</div>;
+};
+
+export default CardsDeal;
---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,31 @@
+import styles from "../styles";
+import { discount, robot } from "../assets";
+
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className={`flex mb:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20% </span>
+            Discount for <span className="text-white">1 Month </span>
+            Accounts
+          </p>
+        </div>
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1>
+            The Next <br className="sm:block hidden" />{" "}
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
